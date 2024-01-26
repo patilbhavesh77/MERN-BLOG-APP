f73bd73869bf3e3282e2f6bb1c4c8e203126212a
patilbhavesh77 <patilbhavesh7788@gmail.com>
2024-01-26 20:23:22 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/Blogdata').then(()=>{
+    console.log("Database is connected")
+}).catch((err)=>{
+    console.log(err);
+});
 
 const app = express();
 
