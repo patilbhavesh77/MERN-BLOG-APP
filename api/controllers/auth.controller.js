20274370254f903ce3e0cc8d7d2091043f10f8fe
patilbhavesh77 <patilbhavesh7788@gmail.com>
2024-01-27 17:33:32 +0530

    add middleware amd a finction to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
 
-export const signup = (req,res) =>{
+export const signup = async(req,res,next) =>{
     const {username , email , password} = req.body;
     if(
         !username ||
@@ -12,7 +13,7 @@ export const signup = (req,res) =>{
         email === '' ||
         password === ''
     ) {
-        return res.status(400).json({ message: 'All fields are required' })
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -27,7 +28,7 @@ export const signup = (req,res) =>{
         newUser.save();
         res.json('Signup successful');
     } catch(error){
-        res.status(500).json({ message: error.message})
+        next(error);
     }
 
     
--- api/index.js
+++ api/index.js
@@ -18,4 +18,13 @@ app.listen(3000,()=>{
 })
 
 app.use('/api/user', userRoutes);
-app.use('/api/auth',authRoutes)
+app.use('/api/auth',authRoutes);
+app.use((err,req,res,next)=>{
+    const statusCode = err.statuscode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    });
+});
